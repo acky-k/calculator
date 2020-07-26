4b596fc37b9f62c7ddcf92bf641e170df67e07a3
acky-k <info@aki-no-shirabe.com>
2020-07-26 22:02:55 +0900

    BMI計算機能実装

--- app/assets/javascripts/culculator.js
+++ app/assets/javascripts/culculator.js
@@ -1,18 +1,36 @@
-$(function() {
+$(function(){
   $("#calc").click(function(){
     var h = Number($("#height").val());
     var w = Number($("#weight").val());
-    $("#price").val( w / ( (h / 100) * (h / 100) ) );
+    $("#price").val(Math.floor(w / ( (h / 100) * (h / 100) ) * 100) / 100)
     var a = $("#price").val();
-      if (a < 16) {
+      if(a == ""){
+      }else if (a < 18.4) {
         $("#image1").fadeIn(1000);
-      }else if (a >= 16 && a < 17) {
+      }else if (a >= 18.5 && a < 24.99) {
         $("#image2").fadeIn(1000);
-      }else if (a >= 17 && a ){
-        
+      }else if (a >= 25 && a < 29.99){
+        $("#image3").fadeIn(1000);
+      }else if (a >= 30 && a < 100){
+        $("#image4").fadeIn(1000);
       }
-}); 
-    
+});
+
+　$("#height").change(function(){
+    $("#image1").hide();
+    $("#image2").hide();
+    $("#image3").hide();
+    $("#image4").hide();
+});
+
+$("#weight").change(function(){
+  $("#image1").hide();
+  $("#image2").hide();
+  $("#image3").hide();
+  $("#image4").hide();
+});
+  　
+
   $("#reload").on('click', function(){
     $("#height").val("");
     $("#weight").val("");
@@ -20,7 +38,6 @@ $(function() {
     $("#image1").hide();
     $("#image2").hide();
     $("#image3").hide();
+    $("#image4").hide();
   });
-
-  
 });
